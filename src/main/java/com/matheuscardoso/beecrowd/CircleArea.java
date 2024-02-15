5a520401f79cbe3eb5d28138833b2901fc71f96e
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-15 14:50:22 -0300

    Adjusting Locale to US

--- src/main/java/com/matheuscardoso/beecrowd/CircleArea.java
+++ src/main/java/com/matheuscardoso/beecrowd/CircleArea.java
@@ -1,10 +1,12 @@
 package com.matheuscardoso.beecrowd;
 
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CircleArea {
     public static void main(String[] args) throws IOException {
+        Locale.setDefault(Locale.US);
         Scanner input = new Scanner(System.in);
         final double pi = 3.14159;
         double lightning = Double.parseDouble(input.nextLine());
