efd8a694d3aa3cd8d58069718ba53e5602ca89b8
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-15 14:48:56 -0300

    Renaming variable n to pi

--- src/main/java/com/matheuscardoso/beecrowd/CircleArea.java
+++ src/main/java/com/matheuscardoso/beecrowd/CircleArea.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class CircleArea {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        final double n = 3.14159;
+        final double pi = 3.14159;
         double lightning = Double.parseDouble(input.nextLine());
-        double area = n * (lightning*lightning);
+        double area = pi * (lightning*lightning);
         System.out.printf("A=%.4f\n", area);
     }
 }
