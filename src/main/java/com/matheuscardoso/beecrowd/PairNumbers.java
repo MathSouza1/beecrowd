8f9c00075b60023a7416c2a5feb3b58bae42fb6d
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-21 21:06:31 -0300

    This challenge is to print the pair numbers from 2 to 100

---
+++ src/main/java/com/matheuscardoso/beecrowd/PairNumbers.java
@@ -0,0 +1,10 @@
+package com.matheuscardoso.beecrowd;
+
+import java.io.IOException;
+
+public class PairNumbers {
+    public static void main(String[] args) throws IOException {
+        for (int i = 2; i <= 100; i += 2)
+            System.out.println(i);
+    }
+}
