9efbec50fe7a4194a18e5ecd7ba9ee2fd1e9f67d
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-29 16:17:50 -0300

    This challenge is to read a number and Print all values that when divided by N gives remainder = 2

---
+++ src/main/java/com/matheuscardoso/beecrowd/RestOfDivision.java
@@ -0,0 +1,24 @@
+package com.matheuscardoso.beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RestOfDivision {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int x = Integer.parseInt(input.nextLine());
+        int y = Integer.parseInt(input.nextLine());
+        int biggest = 0, smallest = 0;
+        if (x > y) {
+            biggest = x;
+            smallest = y;
+        } else if (x < y) {
+            biggest = y;
+            smallest = x;
+        }
+
+        for (int i=smallest+1; i<biggest; i++)
+            if (i % 5 == 2 || i % 5 == 3)
+                System.out.println(i);
+    }
+}
