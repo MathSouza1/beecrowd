9234f4982c6a8e12c9d34885ab4a3b06c4425649
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-02-29 22:42:56 -0300

    This challenge is to read two grades, validate and print the average, if the user want to calculate another average, enter 1 or enter 2 exit

---
+++ src/main/java/com/matheuscardoso/beecrowd/MultipleNotesWithValidation.java
@@ -0,0 +1,42 @@
+package com.matheuscardoso.beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MultipleNotesWithValidation {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        double firstGrade = 0;
+        double secondGrade = 0;
+        while (true) {
+            while(true) {
+                firstGrade = Double.parseDouble(input.nextLine());
+                if (firstGrade < 0 || firstGrade > 10)
+                    System.out.println("nota invalida");
+                else
+                    break;
+            }
+
+            while(true) {
+                secondGrade = Double.parseDouble(input.nextLine());
+                if (secondGrade < 0 || secondGrade > 10)
+                    System.out.println("nota invalida");
+                else
+                    break;
+            }
+
+            System.out.printf("media = %.2f\n", (firstGrade + secondGrade) / 2);
+            String newCalc = "";
+            while (true) {
+                System.out.println("novo calculo (1-sim 2-nao)");
+                newCalc = input.nextLine();
+                if (newCalc.equals("1") || newCalc.equals("2")) {
+                    break;
+                }
+            }
+
+            if (newCalc.equals("2"))
+                break;
+        }
+    }
+}
